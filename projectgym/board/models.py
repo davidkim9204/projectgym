b901ff47a2a1affdd480eec90d3c218d443823ce
DoHyung <davidkim9204@naver.com>
2017-02-16 18:24:17 +0900

    이걸로 다시시작

--- projectgym/board/models.py
+++ projectgym/board/models.py
@@ -5,11 +5,12 @@ from django.utils import timezone
 
 class Board(models.Model):
     title = models.CharField(max_length = 50)
+    photo = models.ImageField()
+    category = models.CharField(max_length = 20)
     instruction = models.TextField()
     outcomes = models.TextField()
     links = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['created_at']
