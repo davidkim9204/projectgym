dff4b7e89283d72ae5aef3376573cfce7a8b0640
DoHyung <davidkim9204@naver.com>
2017-02-17 03:47:37 +0900

    더보기 추가

--- projectgym/board/urls.py
+++ projectgym/board/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns =[
     url(r'^$', views.main),
-    url(r'^list$', views.board_list),
+    url(r'^list/$', views.board_list, name= 'board_list'),
     #url(r'^list$', views.board_new),
     url(r'^(?P<pk>\d+)/$', views.board_detail, name = 'board_detail'),
     #url(r'^(?P<board_slug>[-\w]+)/$', views.board_detail, name = 'board_detail'),
